1681b97f70d9a4845898af0742a996200566fb64
UglyDuck69 <72518028+UglyDuck69@users.noreply.github.com>
2023-10-01 11:56:31 +0400

    d

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("offcanvas").addEventListener("click", function() {
+    var element = document.querySelector(".background2");
+    element.style.display = "none";
+});
+document.getElementById("offcanvas2").addEventListener("click", function() {
+    var element = document.querySelector(".background2");
+    element.style.display = "block";
+});
